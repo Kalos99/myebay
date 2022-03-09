722a5582d58afd1530a54ad4dba41fa320290e1b
Kalos99 <calogerocorsello1999@gmail.com>
2022-03-09 23:03:59 +0100

    Implementazione package model

---
+++ src/main/java/it/prova/myebay/model/Annuncio.java
@@ -0,0 +1,189 @@
+package it.prova.myebay.model;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "annuncio")
+public class Annuncio {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "testo")
+	private String testoAnnuncio;
+	@Column(name = "prezzo")
+	private Integer prezzo;
+	@Column(name = "data")
+	private Date dataPubblicazione;
+	@Column(name = "stato")
+	private Boolean aperto;
+
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "utente_id",nullable = false)
+	private Utente utenteInserimento;
+	
+	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.LAZY)
+	@JoinTable(name = "annuncio_categoria", joinColumns = @JoinColumn(name = "annuncio_id", referencedColumnName = "ID"), inverseJoinColumns = @JoinColumn(name = "categoria_id", referencedColumnName = "ID"))
+	private Set<Categoria> categorie = new HashSet<>(0);
+
+
+	public Annuncio() {
+	}
+
+
+	public Annuncio(Long id, String testoAnnuncio, Integer prezzo, Date dataPubblicazione, Boolean aperto,
+			Utente utenteInserimento, Set<Categoria> categorie) {
+		this.id = id;
+		this.testoAnnuncio = testoAnnuncio;
+		this.prezzo = prezzo;
+		this.dataPubblicazione = dataPubblicazione;
+		this.aperto = aperto;
+		this.utenteInserimento = utenteInserimento;
+		this.categorie = categorie;
+	}
+
+
+	public Annuncio(String testoAnnuncio, Integer prezzo, Date dataPubblicazione, Boolean aperto,
+			Utente utenteInserimento, Set<Categoria> categorie) {
+		this.testoAnnuncio = testoAnnuncio;
+		this.prezzo = prezzo;
+		this.dataPubblicazione = dataPubblicazione;
+		this.aperto = aperto;
+		this.utenteInserimento = utenteInserimento;
+		this.categorie = categorie;
+	}
+
+
+	public Annuncio(String testoAnnuncio, Integer prezzo, Date dataPubblicazione, Boolean aperto) {
+		this.testoAnnuncio = testoAnnuncio;
+		this.prezzo = prezzo;
+		this.dataPubblicazione = dataPubblicazione;
+		this.aperto = aperto;
+	}
+
+
+	public Annuncio(Long id, String testoAnnuncio, Integer prezzo, Date dataPubblicazione, Boolean aperto) {
+		this.id = id;
+		this.testoAnnuncio = testoAnnuncio;
+		this.prezzo = prezzo;
+		this.dataPubblicazione = dataPubblicazione;
+		this.aperto = aperto;
+	}
+
+
+	public Annuncio(String testoAnnuncio, Integer prezzo, Date dataPubblicazione, Boolean aperto,
+			Utente utenteInserimento) {
+		this.testoAnnuncio = testoAnnuncio;
+		this.prezzo = prezzo;
+		this.dataPubblicazione = dataPubblicazione;
+		this.aperto = aperto;
+		this.utenteInserimento = utenteInserimento;
+	}
+
+
+	public Annuncio(Long id, String testoAnnuncio, Integer prezzo, Date dataPubblicazione, Boolean aperto,
+			Utente utenteInserimento) {
+		this.id = id;
+		this.testoAnnuncio = testoAnnuncio;
+		this.prezzo = prezzo;
+		this.dataPubblicazione = dataPubblicazione;
+		this.aperto = aperto;
+		this.utenteInserimento = utenteInserimento;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getTestoAnnuncio() {
+		return testoAnnuncio;
+	}
+
+
+	public void setTestoAnnuncio(String testoAnnuncio) {
+		this.testoAnnuncio = testoAnnuncio;
+	}
+
+
+	public Integer getPrezzo() {
+		return prezzo;
+	}
+
+
+	public void setPrezzo(Integer prezzo) {
+		this.prezzo = prezzo;
+	}
+
+
+	public Date getDataPubblicazione() {
+		return dataPubblicazione;
+	}
+
+
+	public void setDataPubblicazione(Date dataPubblicazione) {
+		this.dataPubblicazione = dataPubblicazione;
+	}
+
+
+	public Boolean getAperto() {
+		return aperto;
+	}
+
+
+	public void setAperto(Boolean aperto) {
+		this.aperto = aperto;
+	}
+
+
+	public Utente getUtenteInserimento() {
+		return utenteInserimento;
+	}
+
+
+	public void setUtenteInserimento(Utente utenteInserimento) {
+		this.utenteInserimento = utenteInserimento;
+	}
+
+	public Set<Categoria> getCategorie() {
+		return categorie;
+	}
+
+
+	public void setCategorie(Set<Categoria> categorie) {
+		this.categorie = categorie;
+	}	
+	
+	public void addToCategorie(Categoria categoriaInstance) {
+		this.categorie.add(categoriaInstance);
+		categoriaInstance.getAnnunci().add(this);
+	}
+
+	public void removeFromCategorie(Categoria categoriaInstance) {
+		this.categorie.remove(categoriaInstance);
+		categoriaInstance.getAnnunci().remove(this);
+	}
+}
---
+++ src/main/java/it/prova/myebay/model/Ruolo.java
@@ -0,0 +1,68 @@
+package it.prova.myebay.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ruolo")
+public class Ruolo implements Comparable<Ruolo>{
+	
+	public static final String ADMIN_ROLE = "ADMIN_ROLE";
+	public static final String CLASSIC_USER_ROLE = "CLASSIC_USER_ROLE";
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "descrizione")
+	private String descrizione;
+	@Column(name = "codice")
+	private String codice;
+
+	public Ruolo() {
+	}
+
+	public Ruolo(String descrizione, String codice) {
+		this.descrizione = descrizione;
+		this.codice = codice;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	@Override
+	public int compareTo(Ruolo o) {
+		if(this.id < o.getId()) {
+			return -1;
+		}
+		if(this.id > o.getId()) {
+			return 1;
+		}
+		return 0;
+	}	
+}
